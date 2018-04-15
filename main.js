4616a5fac3b44b9b83d0f36c1b8ce30b2261e930
mohsinht <l164333@lhr.nu.edu.pk>
2018-04-15 20:22:30 +0500

    major changes

---
+++ main.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBC27pDldUEHDVeDFVzT-2058O77E2kUEc",
+    authDomain: "purzey-b9cbd.firebaseapp.com",
+    databaseURL: "https://purzey-b9cbd.firebaseio.com",
+    projectId: "purzey-b9cbd",
+    storageBucket: "purzey-b9cbd.appspot.com",
+    messagingSenderId: "899372738982"
+  };
+  firebase.initializeApp(config);
+
+
+  
